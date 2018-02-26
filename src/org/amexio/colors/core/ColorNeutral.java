fe223f045e56edc6395fa56193e5877be78ea36e
Araf Karsh Hamid <araf.karsh@gmail.com>
2018-02-26 23:44:33 +0530

    updates on copright

--- src/org/amexio/colors/core/ColorNeutral.java
+++ src/org/amexio/colors/core/ColorNeutral.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018 Araf Karsh Hamid
+ * Copyright (c) MetaMagic Global Inc, NJ, USA
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
